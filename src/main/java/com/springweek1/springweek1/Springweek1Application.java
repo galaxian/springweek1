f90191347799c9e9fc99363673673c0a8e5cc147
galaxian <3ed0324@hanmail.net>
2022-01-24 13:34:03 +0900

    no message

---
+++ src/main/java/com/springweek1/springweek1/Springweek1Application.java
@@ -0,0 +1,13 @@
+package com.springweek1.springweek1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springweek1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springweek1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/springweek1/springweek1/Springweek1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springweek1.springweek1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springweek1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
