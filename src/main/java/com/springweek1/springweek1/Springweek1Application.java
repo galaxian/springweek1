6a80d61fdb97a1474776e789eb240eb319aaf46d
galaxian <3ed0324@hanmail.net>
2022-01-25 15:43:19 +0900

    lucy XSS Servlet filter 적용 및 json 요청에 적용하기 완료

--- src/main/java/com/springweek1/springweek1/Springweek1Application.java
+++ src/main/java/com/springweek1/springweek1/Springweek1Application.java
@@ -1,8 +1,12 @@
 package com.springweek1.springweek1;
 
+import com.navercorp.lucy.security.xss.servletfilter.XssEscapeServletFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
 
 @EnableJpaAuditing
 @SpringBootApplication
@@ -10,6 +14,15 @@ public class Springweek1Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Springweek1Application.class, args);
+    }
+
+    @Bean
+    public FilterRegistrationBean<XssEscapeServletFilter> getFilterRegistrationBean(){
+        FilterRegistrationBean<XssEscapeServletFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new XssEscapeServletFilter());
+        registrationBean.setOrder(1);
+        registrationBean.addUrlPatterns("/*");
+        return registrationBean;
     }
 
 }
